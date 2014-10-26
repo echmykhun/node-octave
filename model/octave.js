7ddc48de050aa46307ea9d3cbb549c0d0465073c
Eugen Chmykhun <e.chmykhun@yandex.ru>
2014-10-26 21:06:14 +0200

    +wrong progress

--- model/octave.js
+++ model/octave.js
@@ -1,4 +1,4 @@
-var sys = require('sys');
+var fs = require('fs');
 var exec = require('shelljs').exec;
 
 
@@ -7,7 +7,18 @@ var octave = {
     output: '',
     execute: function (filename) {
         this.output = exec('octave ' + filename, {'silent': true}).output;
+    },
+    handle: function(content){
+
+        var now = new Date();
+
+        var uniqueFilename = Math.floor(Math.random() * 10) + parseInt(now.getTime()).toString(36).toUpperCase();
+        var filename = '/tmp/' + uniqueFilename + '.m';
+        fs.writeFileSync(filename, content);
+        this.execute(filename);
+        fs.unlink(filename);
     }
+
 
 };
 
--- routes/octave.js
+++ routes/octave.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var octave  = require('../model/octave');
+var octave = require('../model/octave');
 
-router.get('/', function(req, res) {
-    res.render('octave', { input: '', output: '' });
+router.get('/', function (req, res) {
+    res.render('octave', {input: '', output: ''});
 });
 
-router.post('/', function(req, res) {
-    console.log(req.query.input);
-    octave.execute('/home/eugen/doless.m');
-    console.log(octave.output);
-    res.render('octave', { input: '', output: octave.output });
+router.post('/', function (req, res) {
+
+    octave.handle(req.body.input);
+
+    res.render('octave', {input: req.body.input, output: octave.output});
 });
 
 module.exports = router;
